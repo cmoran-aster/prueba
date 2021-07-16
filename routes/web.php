4d28e4a55659d66d91c6e9286d1dcb81256e88de
cmoran-aster <christian_aster@outlook.com>
2021-07-16 13:12:11 -0600

    intentando hace la funcion de auto

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/menu', function () {
+    return view('theme/lte/layout');
+});
+
 Route::get('/dashboard', function () {
+    /*return view('theme/lte/layout');*/
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
